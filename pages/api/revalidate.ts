116252f894db076b79076986077760cbfecf36bb
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-11-29 12:31:01 +0530

    updated api

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res:NextApiResponse) {
     for(const url of req.body){
-        await res.unstable_revalidate(url);
+        await res.revalidate(url);
     }
     res.status(200).json({ revalidate: true });
 }
